c0643431e9f8ffd2dd3264c67e13cffe757105f1
Bidisha-Roy <78555584+Bidisha-Roy@users.noreply.github.com>
2022-04-08 15:45:29 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 15:30:45 2022
+
+@author: user
+"""
+
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
